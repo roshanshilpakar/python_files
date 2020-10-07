ba1f869f7c6601c446351b206d8b1dde8244effb
Roshan Shilpakar <roshan.shilpakar.2@gmail.com>
2020-10-07 18:51:25 +0545

    news_notifier

---
+++ news_notifier_using_python.py
@@ -0,0 +1,13 @@
+import feedparser
+from player import notification
+import time
+
+def parseFeed():
+    f = feedparser.parse("http://feeds.bci.co.np/news/rss.xml")
+    for newsitem in f['items']:
+        notification.notify(title=newsitem['title'], message = str(newsitem['summary']), timeout = 150)
+        time.sleep(1200)
+
+
+if __name__ == '_main_':
+    parseFeed()
