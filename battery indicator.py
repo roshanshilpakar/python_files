8a55fb7d75692d16939111136d68ed9ae8039e8a
Roshan Shilpakar <roshan.shilpakar.2@gmail.com>
2020-10-07 19:09:59 +0545

    done

--- battery indicator.py
+++ battery indicator.py
@@ -1,2 +1,9 @@
 import psutil
-print(psutil.sensors_battery())
+from plyer import notification
+import time
+
+battery = psutil.sensors_battery()
+while True:
+    percent = battery.percent
+    notification.notify(title= "Battery Percentage", message = str(percent)+ "% Battert remaining", timeout = 100)
+    time.sleep(60*60) 
